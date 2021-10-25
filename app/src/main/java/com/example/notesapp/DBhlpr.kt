76eb0e0cbc67cd1fd012768cbd16d1c129b005f5
AhadAlowairdhi <90708622+AhadAlowairdhi@users.noreply.github.com>
2021-10-25 16:26:34 +0300

    Update DBhlpr.kt

--- app/src/main/java/com/example/notesapp/DBhlpr.kt
+++ app/src/main/java/com/example/notesapp/DBhlpr.kt
@@ -17,10 +17,23 @@ class DBhlpr(context: Context) : SQLiteOpenHelper(context, "notesapp.db", null, 
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {}
+    
     fun saveData(note: String): Long{
         val cv = ContentValues()
         cv.put("Note",note)
         var status =  sqLiteDatabase.insert("notes",null,cv)
         return status
+    }
+    
+    @SuppressLint("Range")
+    fun retriveAll():ArrayList<String>{
+        var al = arrayListOf<String>()
+        var c : Cursor = sqLiteDatabase.query("notes",null,null, null,null,null,null)
+        if (c.moveToFirst()) {
+            do {
+                al.add(c.getString(c.getColumnIndex("Note")))
+            } while (c.moveToNext())
+        }
+        return al
     }
 }

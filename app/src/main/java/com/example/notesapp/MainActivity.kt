85916331817761d4cd4885379312d3e3af2b4c35
AhadAlowairdhi <90708622+AhadAlowairdhi@users.noreply.github.com>
2021-10-25 16:30:24 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -13,18 +13,23 @@ class MainActivity : AppCompatActivity() {
     lateinit var rvNotes: RecyclerView
     lateinit var notesList: ArrayList <String>
     lateinit var edtNote: EditText
+    lateinit var dbh : DBhlpr
 
     var note = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        dbh = DBhlpr(applicationContext)
 
         edtNote=findViewById(R.id.edtNote)
 
         rvNotes=findViewById(R.id.rvNotes)
         notesList = arrayListOf()
+        notesList = dbh.retriveAll()
         rvNotes.adapter = NotesAdapter(notesList)
         rvNotes.layoutManager = LinearLayoutManager(this)
+        rvNotes.adapter?.notifyDataSetChanged()
+        rvNotes.scrollToPosition(notesList.size-1)
 
 
 
@@ -35,14 +40,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun addNote(){
-        note=edtNote.text.toString()
+        try{
+            note=edtNote.text.toString()
+            var status = dbh.saveData(note)
+            Toast.makeText(applicationContext, "Note submitted successfully! "+status, Toast.LENGTH_LONG).show()
+            notesList.add(note)
+            rvNotes.adapter?.notifyDataSetChanged()
+            edtNote.text.clear()
+            edtNote.clearFocus()
+            rvNotes.scrollToPosition(notesList.size-1)
 
-        var dbh = DBhlpr(applicationContext)
-        var status = dbh.saveData(note)
-        Toast.makeText(applicationContext, "Note submitted successfully! "+status, Toast.LENGTH_LONG).show();
-
-        rvNotes.adapter?.notifyDataSetChanged()
-        edtNote.text.clear()
-        edtNote.clearFocus()
+        }catch (e:Exception){
+            Toast.makeText(applicationContext, "There is no notes, in DB", Toast.LENGTH_SHORT).show()
+        }
     }
 }
